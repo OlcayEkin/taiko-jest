a2213111831680be8e27709380762c10a14d7827
OlcayEkin <olcayekinn@gmail.com>
2019-12-09 21:39:20 +0300

    Fixing const

--- __tests__/jest-test.js
+++ __tests__/jest-test.js
@@ -1,4 +1,4 @@
-const { openBrowser, write, closeBrowser, goto, press, text, focus, inputField, toRightOf, intercept } = require('taiko');
+const { openBrowser, write, closeBrowser, goto, press, text, focus, inputField, toRightOf, intercept, click, listItem, below } = require('taiko');
 
 describe('Getting Started with Jest and Taiko', () => {
 
